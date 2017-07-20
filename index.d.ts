21766537932420e34fecac46028d1b0b2bcdf1ca
Santosh Anand <santoshanand.jogi@gmail.com>
2017-07-20 23:20:48 +0530

    Added index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,3 @@
 export { Auth } from './lib/auth';
-export { Apex } from './lib/api/apex';
+export { Apex } from './lib/api/apex'; 
 export { IAuth, IUser, IWebAuth, IToken } from './lib/interfaces';
